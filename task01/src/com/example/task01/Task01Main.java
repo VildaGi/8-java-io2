37c1165e3aacb0ccb98540b2db7c9b0b525f7b62
Vildan_Gimatov <vildangimatov192@gmail.com>
2025-11-18 09:35:25 +0500

    12

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,34 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String ffprobePath = "D:/Dev/JavaProjects/8-java-io2/ffmpeg-2025-11-17-git-e94439e49b-full_build/bin/ffprobe.exe";
+        String mp3Path = file.getPath();
+
+        Process process = new ProcessBuilder(ffprobePath, "-v", "error", "-of", "flat", "-show_format", mp3Path).start();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+            String line;
+            while((line = br.readLine()) != null){
+                if (line.startsWith("format.tags.title")){
+                    return line.split("=")[1].replace("\"", "");
+                }
+            }
+        }
+        return "exception";
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,47 @@
 package com.example.task02;
 
+import org.graalvm.compiler.api.replacements.Snippet;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        List<Path> paths = new ArrayList<>();
+        Stack<Path> stack = new Stack<>();
+        stack.add(rootDir);
 
-        return null;
+        while(!stack.empty()){
+            Path currentPath = stack.pop();
+            if (currentPath.toFile().isFile()){
+                paths.add(currentPath);
+            } else {
+                stack.addAll(getFiles(currentPath));
+            }
+        }
+
+
+        return paths;
+    }
+
+    public static ArrayList<Path> getFiles(Path pathToFile){
+        File file = pathToFile.toFile();
+        ArrayList<Path> pathArrayList = new ArrayList<>();
+
+        Arrays.stream(file.listFiles()).map(File::toPath).forEach(pathArrayList::add);
+        return pathArrayList;
     }
 }
